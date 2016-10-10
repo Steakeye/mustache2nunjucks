15f9b2912afa94b9afa9cbc75373b7c844d37a55
Andrew Keats <andrew.keats@digital.hmrc.gov.uk>
2016-10-10 17:47:09 +0100

    setting values properly

--- src/index.ts
+++ src/index.ts
@@ -10,28 +10,38 @@ import cliArgs = require('commander');
 //import filesCopier = require('copyfiles');
 
 var files = process.argv.slice(2);
+let source:string = process.cwd();
+let target:string = source
 
-files.forEach(function (file) {
+function setSource(aPath:string) {
+    source = aPath
+}
+function setTarget(aPath:string) {
+    target = aPath
+}
+/*files.forEach(function (file) {
     var inStream = fs.createReadStream(file);
     var outStream = fs.createWriteStream(file + '.js');
 
-    outStream.write('module.exports = \'');n
+    outStream.write('module.exports = \'');
 
     inStream.pipe(through(function (buf) {
         this.queue((buf + '').replace(/'/g, '\\\'').replace(/\r\n|\r|\n/g, '\\n'));
     }, function () {
         this.queue('\';');
     })).pipe(outStream);
-});
+});*/
 
 //Set up the CLI interface then process the arguments in order to get the data/instructions
 cliArgs.version('0.0.1')
-    .option('-s, --source', 'TODO')
-    .option('-o, --output', 'TODO')
+    .option('-s, --source [path]', 'Directory or file to convert', setSource)
+    .option('-o, --output [path]', 'Location to save converted file(s)', setTarget)
     .parse(process.argv);
 
 function ConvertFiles() {
-    console.log("CWD:", process.cwd())
+    console.log("source:", source)
+    console.log("target:", target)
+    console.log("files?:", files)
 }
 
 ConvertFiles();
